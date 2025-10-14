02dfde9381ff00361605b2d3478954f983d7bb5d
ISA-v-bit <vnvstefania@gmail.com>
2025-10-14 19:36:25 +0300

    Update form.js

--- telegram-webapp/lib/form.js
+++ telegram-webapp/lib/form.js
@@ -1,1 +1,9 @@
-
+export async function postJSON(url, data) {
+  const res = await fetch(url, {
+    method: 'POST',
+    headers: { 'Content-Type':'application/json' },
+    body: JSON.stringify(data)
+  });
+  if (!res.ok) throw new Error(await res.text().catch(()=>res.statusText));
+  return res.json().catch(()=>({ok:true}));
+}
