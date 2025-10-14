a87294a7f97841c8018e67032c44e9071d0236b8
ISA-v-bit <vnvstefania@gmail.com>
2025-10-14 19:33:12 +0300

    Create cta.js

---
+++ telegram-webapp/components/cta.js
@@ -0,0 +1,8 @@
+export function renderCTA(el) {
+  el.innerHTML = `
+    <div class="container cta" id="apply">
+      <h3>Готов прокачать мышление?</h3>
+      <button class="btn btn-primary" data-open-modal>Записаться на встречу</button>
+    </div>
+  `;
+}
